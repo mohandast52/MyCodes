651aad0e1d602234ea7211d934e88b7d99b42c7d
Mohan Das <mohandast52@gmail.com>
2018-08-06 15:05:49 +0530

    added split Screen Slider, split landing page and mouse tracker html pages

--- bundle.js
+++ bundle.js
@@ -1,9 +1,11 @@
+
+// only repeatedly used files are bundled here!! 
+/*
+1. jquery 3.3.1
+2. bootstrap 4
+*/
+
 window.jQuery = global.$ = $ = require('jquery');
 window.Popper = require('popper.js');
-// require("./node_modules/bootstrap/dist/css/bootstrap.css");
-// var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap.js');
-// bootstrap = require("bootstrap-sass");
-// console.log(bootstrap);
-
 bootstrap = require('bootstrap');
 // console.log(bootstrap);
---
+++ js/swiperJS.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper('.swiper-container-1', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+var swiper = new Swiper('.swiper-container-2', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
